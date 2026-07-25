7e9c796dd37ae8656e11db81c61f4df43422c335
nomadhermit <franzdep@gmail.com>
2026-07-25 16:34:12 -0400

    test mm file

--- maps/test.mm
+++ maps/test.mm
@@ -1,6 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <map version="1.0.1">
-  <node TEXT="Core Node" ID="ID_dk7t74ypn2bp" CREATED="1784998639237" MODIFIED="1784998639237">
-    <node TEXT="test child" ID="ID_ms0m3h5avwh7" CREATED="1784998639237" MODIFIED="1784998639237"></node>
+  <node TEXT="example family notes" ID="ID_dk7t74ypn2bp" CREATED="1785011563559" MODIFIED="1785011563559">
+    <node TEXT="Jane Joe" ID="ID_ms0m3h5avwh7" CREATED="1785011563559" MODIFIED="1785011563559">
+      <node TEXT="married to John Doe" ID="ID_ms0tqojhxv08" CREATED="1785011563559" MODIFIED="1785011563559">
+        <arrowlink DESTINATION="ID_ms0trpjujj4o" COLOR="#0000ff"></arrowlink>
+      </node>
+      <node TEXT="children" ID="ID_ms0tpn1ofwky" CREATED="1785011563559" MODIFIED="1785011563559">
+        <node TEXT="Mary " ID="ID_ms0tq1bmfx7j" CREATED="1785011563559" MODIFIED="1785011563559"></node>
+        <node TEXT="Bob" ID="ID_ms0tqgqtcpi5" CREATED="1785011563559" MODIFIED="1785011563559"></node>
+      </node>
+    </node>
+    <node TEXT="John Smith" ID="ID_ms0toipn3zsu" CREATED="1785011563559" MODIFIED="1785011563559">
+      <node TEXT="married to Mary Jane" ID="ID_ms0toeoyjcgo" CREATED="1785011563559" MODIFIED="1785011563559"></node>
+      <node TEXT="children" ID="ID_ms0trlbwq128" CREATED="1785011563559" MODIFIED="1785011563559">
+        <node TEXT="John Doe" ID="ID_ms0trpjujj4o" CREATED="1785011563559" MODIFIED="1785011563559">
+          <arrowlink DESTINATION="ID_ms0tqojhxv08" COLOR="#0000ff"></arrowlink>
+        </node>
+      </node>
+    </node>
   </node>
 </map>
