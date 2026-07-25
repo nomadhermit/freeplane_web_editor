c9391ec09adb20e34a96b8d58221aacd7a5eb22c
nomadhermit <franzdep@gmail.com>
2026-07-25 13:11:14 -0400

    Delete static/test.mm

--- static/test.mm
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<map version="1.0.1">
-  <node TEXT="Core Node" ID="ID_dk7t74ypn2bp" CREATED="1784998639237" MODIFIED="1784998639237">
-    <node TEXT="test child" ID="ID_ms0m3h5avwh7" CREATED="1784998639237" MODIFIED="1784998639237"></node>
-  </node>
-</map>
